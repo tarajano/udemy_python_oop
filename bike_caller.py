138c6465a837856e04dd4150474cb668e26dab1e
Manuel A. Alonso Tarajano <tarajano@gmail.com>
2018-09-30 07:53:33 -0400

    adds abstract class bike_abc and other associated 'bike' classes and callers.

---
+++ bike_caller.py
@@ -0,0 +1,14 @@
+
+from bike_mtb import *
+
+
+mtb = MountainBike('chromed')
+
+print(mtb.get_wheels())
+print(mtb.get_type())
+print(mtb.get_color())
+mtb.set_color('green')
+print(mtb.get_color())
+
+
+
