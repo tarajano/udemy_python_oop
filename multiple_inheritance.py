1912a18cd28b96677320bc5266a2f648c4c5092a
Manuel A. Alonso Tarajano <tarajano@gmail.com>
2018-10-05 22:38:49 -0400

    ...

--- multiple_inheritance.py
+++ multiple_inheritance.py
@@ -10,9 +10,21 @@ Here I will create a multiple inheritance situation to test.
 """ 
 
 class GrandGrandParent(object):
+
+    def __init__(self, value = None):
+        self.specie = 'Human'
+        if not value:
+            self.origin = 'African'
+
     @classmethod # Class method Decorator
     def iam(cls):
         return cls.__name__
+
+    def get_origin(self):
+        return self.origin
+
+    def get_specie(self):
+        return self.specie
     
     @classmethod
     def get_origin(cls):
@@ -22,26 +34,32 @@ class GrandGrandParent(object):
     def get_specie():
         return 'Human'
 
-
 class ParentA(GrandGrandParent):
-    
-    def get_origin(self):
-        return 'European' 
-        
+    def __init__(self, value = None):
+        if not value:
+            self.origin = 'Asian'
+
+        super().__init__(self.origin)
+        self.hair_color = 'Black'
+
 
 class ParentB(GrandGrandParent):
-
-    def get_origin(self):
-        return 'Arabic' 
+    def __init__(self, value = None):
+        if not value:
+            self.origin = 'Nordic'
+             
+        super().__init__(self.origin)
+        self.hair_color = 'Blond'
 
 
 class Child(ParentA, ParentB):
-    pass
+    def __init__(self, value = None):
+        self.origin = value
+        super().__init__(self.origin)
 
 
-c = Child()
+c = Child('Latin')
 print(c.iam())
 print(c.get_origin())
 print(c.get_specie())
 print(Child.mro())
-
