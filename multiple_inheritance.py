fb62e46e05352faff91397382a7573bcf4014f56
Manuel A. Alonso Tarajano <tarajano@gmail.com>
2018-10-05 22:28:22 -0400

    improves multiple_inheritance

--- multiple_inheritance.py
+++ multiple_inheritance.py
@@ -10,39 +10,51 @@ Here I will create a multiple inheritance situation to test.
 """ 
 
 class GrandGrandParent(object):
+
+    def __init__(self, value = None):
+        self.specie = 'Human'
+        if not value:
+            self.origin = 'African'
+
     @classmethod # Class method Decorator
     def iam(cls):
         return cls.__name__
-    
-    def get_origin(cls):
-        return 'African'
 
-    @staticmethod # Static is used for 'utility'  methods
-    def get_specie(cls):
-        return 'Human'
+    def get_origin(self):
+        return self.origin
+
+    def get_specie(self):
+        return self.specie
 
 
 class ParentA(GrandGrandParent):
-    
-    def get_origin(self):
-        return 'European' 
-        
+    def __init__(self, value = None):
+        if not value:
+            self.origin = 'Asian'
+
+        super().__init__(self.origin)
+        self.hair_color = 'Black'
+
 
 class ParentB(GrandGrandParent):
-
-    def get_origin(self):
-        return 'Arabic' 
+    def __init__(self, value = None):
+        if not value:
+            self.origin = 'Nordic'
+             
+        super().__init__(self.origin)
+        self.hair_color = 'Blond'
 
 
 class Child(ParentA, ParentB):
-    pass
+    def __init__(self, value = None):
+        self.origin = value
+        super().__init__(self.origin)
 
 
-c = Child()
+c = Child('Latin')
 print(c.iam())
 print(c.get_origin())
 print(c.get_specie())
 print(Child.mro())
 
 
-# TODO .. INVESTIGATE DIFFERENCE BETWEEN @CLASSMETHOD AND @STATICMETHOD decorators
