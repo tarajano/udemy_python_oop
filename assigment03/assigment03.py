f20501de4db77eebe417db231974b690fc91cf1b
Manuel A. Alonso Tarajano <tarajano@gmail.com>
2018-10-16 22:15:41 -0400

    Improves assignment03

--- assigment03/assigment03.py
+++ assigment03/assigment03.py
@@ -1,24 +1,30 @@
 
 
-class DoThis(dict):
-    """docstring for DoThis"""
+class ConfigDict(dict):
+    """docstring for ConfigDict"""
     def __init__(self):
-        super(DoThis, self).__init__()
+        super(ConfigDict, self).__init__()
+        self._load_config()
 
     def __setitem__(self, key, value):
         dict.__setitem__(self, key, value)
+        self._write_config_file()
 
-    def set_config(self):
+    def _write_config_file(self):
         with open('dothis.config', 'w') as dtc:
             for (key, value) in self.items():
                 dtc.write("{}={}\n".format(key, value))
 
-    def print_config(self):
+    def print_config_file(self):
         with open('dothis.config', 'r') as dtc:
             for line in dtc.readlines():
-                print(line)
+                print(line.strip())
+    
+    def print_dict(self):
+        for (key, value) in self.items():
+            print("{} -> {}".format(key, value))
 
-    def load_config(self):
+    def _load_config(self):
         with open('dothis.config', 'r') as dtc:
             for line in dtc.readlines():
                 (key, value) = line.strip().split('=')
--- assigment03/caller.py
+++ assigment03/caller.py
@@ -1,11 +1,28 @@
 
 
-from assigment03 import DoThis
+from assigment03 import ConfigDict
+import sys
+
+# Run this script like:
+# caller.py OR
+# caller.py <key> <value>
+
 
 # Reading key,pair values from config file
-dt = DoThis()
-dt.load_config()
-print(dt)
-dt['marc'] = 'alon'
-dt.set_config()
+dt = ConfigDict()
 
+if 1 == len(sys.argv):
+    dt.print_dict()
+elif 3 == len(sys.argv):
+    dt[str(sys.argv[1])] = str(sys.argv[2])
+else:
+    print("""
+            Run this script like:
+            python caller.py
+            OR
+            python caller.py <key> <value>
+        """)
+    exit()
+
+print("dt.print_dict()")
+dt.print_dict()
