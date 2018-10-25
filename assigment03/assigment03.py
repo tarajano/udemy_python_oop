dc293036130bae49fd0c538939f6fe2a4e15be7d
Manuel A. Alonso Tarajano <tarajano@gmail.com>
2018-10-24 22:52:34 -0400

    completes exception class ConfigKeyError.

--- assigment03/assigment03.py
+++ assigment03/assigment03.py
@@ -1,32 +1,74 @@
 
+import os
 
 class ConfigDict(dict):
     """docstring for ConfigDict"""
-    def __init__(self):
+    config_file = None
+
+    def __init__(self, filename):
         super(ConfigDict, self).__init__()
+        self.config_file = filename
         self._load_config()
 
     def __setitem__(self, key, value):
         dict.__setitem__(self, key, value)
         self._write_config_file()
+    
+    def __getitem__(self, key):
+        if not key in self:
+            raise ConfigKeyError(self, key)
+        return dict.__getitem__(self, key)
+
+    def __test_file_access(self, mode):
+        # mode: 'r', 'w'
+        try:
+            with open(self.config_file, mode) as dtc:
+                pass
+        except IOError as err:
+            print('ERROR: {0}'.format(str(err)))
+            exit()
 
     def _write_config_file(self):
-        with open('dothis.config', 'w') as dtc:
+        self.__test_file_access('w')
+        with open(self.config_file, 'w') as dtc:
             for (key, value) in self.items():
-                dtc.write("{}={}\n".format(key, value))
+                dtc.write("{0}={1}\n".format(key, value))
 
     def print_config_file(self):
-        with open('dothis.config', 'r') as dtc:
+        self.__test_file_access('r')
+        with open(self.config_file, 'r') as dtc:
             for line in dtc.readlines():
                 print(line.strip())
     
     def print_dict(self):
         for (key, value) in self.items():
-            print("{} -> {}".format(key, value))
+            print("{0} -> {1}".format(key, value))
 
     def _load_config(self):
-        with open('dothis.config', 'r') as dtc:
+        self.__test_file_access('r')
+        with open(self.config_file, 'r') as dtc:
             for line in dtc.readlines():
-                (key, value) = line.strip().split('=')
+                (key, value) = line.strip().split('=', 1)
                 if key:
-                    self.__setitem__(key, value) 
+                    self.__setitem__(key, value)
+
+
+class ConfigKeyError(Exception):
+    """docstring for ConfigKeyError"ConfigDict, """
+    def __init__(self, *args):
+        # super(ConfigKeyError, ConfigDict,  self).__init__()
+        self.good_keys = list(args[0].keys())
+        self.bad_key = args[1]
+
+    def __str__(self):
+         return 'Key "{}" is not present in dictionary.\nAvailable keys are: {}'.format(self.bad_key, ' '.join(self.good_keys))
+
+
+
+
+
+
+
+
+
+        
--- assigment03/caller.py
+++ assigment03/caller.py
@@ -1,5 +1,7 @@
 
 
+# Generating custom exceptions  
+
 from assigment03 import ConfigDict
 import sys
 
@@ -7,22 +9,21 @@ import sys
 # caller.py OR
 # caller.py <key> <value>
 
-
 # Reading key,pair values from config file
-dt = ConfigDict()
+dt = ConfigDict('dothis.config')
 
-if 1 == len(sys.argv):
-    dt.print_dict()
-elif 3 == len(sys.argv):
-    dt[str(sys.argv[1])] = str(sys.argv[2])
-else:
-    print("""
-            Run this script like:
-            python caller.py
-            OR
-            python caller.py <key> <value>
-        """)
-    exit()
 
-print("dt.print_dict()")
-dt.print_dict()
+# if 1 == len(sys.argv):
+#     dt.print_dict()
+# elif 3 == len(sys.argv):
+#     dt[str(sys.argv[1])] = str(sys.argv[2])
+# else:
+#     print("""
+#             Run this script like:
+#             python caller.py
+#             OR
+#             python caller.py <key> <value>
+#         """)
+#     exit()
+
+print(dt['aseb'])
